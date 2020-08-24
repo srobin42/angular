c4f800b015661bc81801edeaa1aaa6b16c531b91
Steph Eikenberry <srobin@zhar.net>
2020-08-23 19:41:55 -0700

    Update app.module with new components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CartService } from './cart/cart.service';
+import { ConcessionComponent } from './concession/concession.component';
+import { ConcessionService } from './concession/concession.service';
+import { InMemoryConcessionService } from './in-memory-concession.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ConcessionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryConcessionService)
   ],
-  providers: [],
+  providers: [
+    ConcessionService,
+    CartService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
