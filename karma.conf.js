f50a3e2d0ed231377efa99f50df576a8627b51af
Steph Eikenberry <srobin@zhar.net>
2020-08-29 18:37:31 -0700

    Properly configure app to run tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,9 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
