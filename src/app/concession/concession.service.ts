0556176aa4ba1dabc52dffb44a229320f5fd1adf
Steph Eikenberry <srobin@zhar.net>
2020-08-30 14:03:01 -0700

    Remove unneeded method in concession.service

--- src/app/concession/concession.service.ts
+++ src/app/concession/concession.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Concession } from './concession';
@@ -7,7 +7,6 @@ import { Concession } from './concession';
 @Injectable()
 export class ConcessionService {
   private url = 'api/concessions';
-  private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(
     private http: HttpClient
@@ -16,10 +15,4 @@ export class ConcessionService {
   public getConcessions(): Observable<Concession[]> {
     return this.http.get<Concession[]>(this.url);
   }
-
-  public addConcession(concessionName: string, concessionPrice: number): Observable<Concession[]> {
-    const newConcession: string = JSON.stringify({ name: concessionName, price: Number(concessionPrice), image: ''});
-    return this.http.post<Concession[]>(this.url, newConcession, { headers: this.headers });
-  }
-
 }
